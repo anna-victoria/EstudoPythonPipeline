9f20006e5349d78d055b72fce7762ceed0aac341
Anna Victoria Feliciana <anna.feliciana@upe.br>
2024-09-11 18:58:41 -0300

    Mudando valores

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
 import streamlit as st
-import contrato
+from contrato import Vendas
+from contrato import ProdutoEnum
 from datetime import datetime, time
+from pydantic import ValidationError
+
+
+import contrato
 
 def main():
-    st.title("Sistema de CRM e Vendas - Frontend")
+    st.title("Sistema de CRM e Vendas")
     email = st.text_input("Email do vendedor")
     data = st.date_input("Data da compra", datetime.now())
     hora = st.time_input("Hora da compra", value=time(9, 0))  # Valor padrão: 09:00
     valor = st.number_input("Valor da venda")
-    quantidade = st.number_input("Quantidade de produtos")
-    produto = st.selectbox("Selecione o produto vendido", ["Zapflow com Gemini" , "Zapflow com ChatGPT", "Zapflow com Lhama"]) 
+    quantidade = st.number_input("Quantidade de produtos", min_value=1, step=1)
+    produto = st.selectbox("Selecione o produto vendido", ["Produto 1" , "Produto 2", "Produto 3"]) 
     
     if st.button("Salvar"):
-        data_hora = datetime.combine(data, hora)
-        st.write("Dados da venda:")
-        st.write(f"Email do vendedor: {email}")
-        st.write(f"Data da venda: {data}")
-        st.write(f"Hora da venda: {hora}")
-        st.write(f"Valor da venda: {valor}")
-        st.write(f"Quantidade de produtos: {quantidade}")
-        st.write(f"Produto vendido: {produto}")
+        
+        try:        
+            data_hora = datetime.combine(data, hora)
+            # Esse data_hora é um objeto datetime que combina a data e a hora que o usuário inseriu
+            
+            venda = Vendas(email=email, data=data, hora=hora, valor=valor, quantidade=quantidade, produto=produto) 
+            # Aqui, estamos criando uma instância da classe Vendas, que foi definida no arquivo contrato.py
+            
+            st.write(venda)
+                        
+            # st.write("Dados da venda:")
+            # st.write(f"Email do vendedor: {email}")
+            # st.write(f"Data da venda: {data}")
+            # st.write(f"Hora da venda: {hora}")
+            # st.write(f"Valor da venda: {valor}")
+            # st.write(f"Quantidade de produtos: {quantidade}")
+            # st.write(f"Produto vendido: {produto}")
+        
+        except ValidationError as e:
+            st.error(f"Erro de validação: {e}")
         
        
 if __name__ == '__main__':
--- contrato.py
+++ contrato.py
@@ -1,21 +1,17 @@
 from datetime import datetime
 from typing import Tuple
-from pydantic import BaseModel, EmailStr, validade_call
+from pydantic import BaseModel, EmailStr, PositiveFloat, PositiveInt, validate_call, validator
 from enum import Enum
 
 class ProdutoEnum (str, Enum):
-    produto1 = "Zapflow com Gemini"
-    produto2 = "Zapflow com ChatGPT"
-    produto3 = "Zapflow com Lhama"
+    produto1 = "Produto 1"
+    produto2 = "Produto 2"
+    produto3 = "Produto 3"
     
 
 class Vendas(BaseModel):
     email: EmailStr
     data: datetime
-    valor: float    
-    quantidade: int
+    valor: PositiveFloat
+    quantidade: PositiveInt
     produto: ProdutoEnum
-    
-    @validade_call('produto')
-    def categoria_no_enum(cls, v):
-        return v
